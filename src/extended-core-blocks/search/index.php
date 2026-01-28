61a5f630f6ef6f4468b36bda5ad8f09d329da0b6
RobLoweMOJ <robert.lowe@digital.justice.gov.uk>
2026-01-28 23:06:17 +0000

    Search Block - Tidy Code

--- src/extended-core-blocks/search/index.php
+++ src/extended-core-blocks/search/index.php
@@ -13,8 +13,7 @@ function wb_extend_search_block( $block_content, $block ) {
 
     $block_content = str_replace( __('Search this site'), __('Search') . ' ' . $site_name, $block_content );
 
-     $custom_svg = '
-    <svg class="wb-search-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg>';
+    $custom_svg = '<svg class="wb-search-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg>';
 
     // Replace the default WordPress SVG with yours
     $block_content = preg_replace(
