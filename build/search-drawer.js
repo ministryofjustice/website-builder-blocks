23bd1548ed59aa2ed8f3286cde8fac846799127d
RobLoweMOJ <robert.lowe@digital.justice.gov.uk>
2026-01-29 13:23:06 +0000

    Search Block - improvements

--- build/search-drawer.js
+++ build/search-drawer.js
@@ -4,13 +4,13 @@
  */
 
 const header = document.querySelector("header");
-const headerSearchBlockWrapper = document.querySelector("header .wb-search-block-wrapper");
-const headerSearchToggle = document.querySelector("header .wb-search-toggle");
-const headerSearchToggleButton = document.querySelector("header .wb-search-toggle .wp-block-search-toggle-button");
-const headerSearchFormWrapper = document.querySelector("header .wb-search-form-wrapper");
-const headerSearchForm = document.querySelector("header .wb-search-form-wrapper .wp-block-search");
-const headerSearchLabel = document.querySelector("header .wb-search-form-wrapper .wp-block-search__label");
-const headerSearchInsideWrapper = document.querySelector("header .wb-search-form-wrapper .wp-block-search__inside-wrapper");
+const headerSearchBlockWrapper = header.querySelector(".wb-search-block-wrapper");
+const headerSearchToggle = header.querySelector(".wb-search-toggle");
+const headerSearchToggleButton = header.querySelector(".wb-search-toggle .wp-block-search-toggle-button");
+const headerSearchFormWrapper = header.querySelector(".wb-search-form-wrapper");
+const headerSearchForm = header.querySelector(".wb-search-form-wrapper .wp-block-search");
+const headerSearchLabel = header.querySelector(".wb-search-form-wrapper .wp-block-search__label");
+const headerSearchInsideWrapper = header.querySelector(".wb-search-form-wrapper .wp-block-search__inside-wrapper");
 
 // store original inline width (if any) as this is set by block settings
 const originaSearchWidth = headerSearchInsideWrapper ? headerSearchInsideWrapper.style.width : "";
@@ -49,14 +49,15 @@ function toggleSearchDrawer() {
 }
 
 function openSearchDrawer() {
-    const height = headerSearchFormWrapper.offsetHeight;
-    header.style.marginBottom = `${height}px`;
     headerSearchFormWrapper.classList.add("is-layout-constrained");
     headerSearchLabel.classList.remove("screen-reader-text");
     headerSearchForm.classList.remove("wp-block-search__button-inside");
     headerSearchForm.classList.add("wp-block-search__button-outside");
     headerSearchInsideWrapper.style.width = "";
     headerSearchToggleButton.setAttribute("aria-label", "Close search");
+    
+    const height = headerSearchFormWrapper.offsetHeight;
+    header.style.marginBottom = `${height}px`;
 }
 
 function closeSearchDrawer() {
--- src/extended-core-blocks/search/index.php
+++ src/extended-core-blocks/search/index.php
@@ -24,7 +24,7 @@ function wb_extend_search_block( $block_content, $block ) {
 
     $block_content = '<div class="wb-search-form-wrapper">' . $block_content . '</div>';
 
-    $block_content = '<div class="wb-search-toggle" aria-hidden="true"><button aria-label="Open search" class="wp-block-search-toggle-button wp-element-button" type="submit"><svg class="wb-search-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg><svg class="wb-close-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></button></div>' . $block_content;
+    $block_content = '<div class="wb-search-toggle" aria-hidden="true"><button aria-label="Open search" class="wp-block-search-toggle-button wp-element-button" type="submit">' . $custom_svg . '<svg class="wb-close-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></button></div>' . $block_content;
 
     $wrapper_classes = '';
     if(!empty($block['attrs']['className'])){
