b6b4b592746a8361c43453e9ca82a3819bb88b57
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2026-02-20 16:20:43 +0000

    Spacing progress

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '6f940dc59b314d483271');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '5f00b13970867201a8a7');
--- src/extended-core-blocks/query-pagination/index.jsx
+++ src/extended-core-blocks/query-pagination/index.jsx
@@ -1,4 +1,5 @@
 import { registerBlockStyle } from "@wordpress/blocks";
+import { addFilter } from "@wordpress/hooks";
 import { __ } from "@wordpress/i18n";
 
 /**
@@ -11,3 +12,62 @@ registerBlockStyle("core/query-pagination", {
   name: "centred-on-mobile",
   label: __("Centred on mobile", "wb_blocks"),
 });
+const { PanelBody } = wp.components;
+const { InspectorControls } = wp.blockEditor;
+const { createHigherOrderComponent } = wp.compose;
+const { Fragment } = wp.element;
+const { __experimentalUnitControl: UnitControl } = wp.components;
+
+const withQueryPaginationGapControl = createHigherOrderComponent(
+    (BlockEdit) => {
+        return (props) => {
+
+            if (props.name !== "core/query-pagination") {
+                return <BlockEdit {...props} />;
+            }
+
+            const currentGap =
+                props.attributes.style?.spacing?.blockGap || "0";
+
+            const updateGap = (newValue) => {
+                props.setAttributes({
+                    style: {
+                        ...props.attributes.style,
+                        spacing: {
+                            ...props.attributes.style?.spacing,
+                            blockGap: newValue,
+                        },
+                    },
+                });
+            };
+
+            return (
+                <Fragment>
+                    <BlockEdit {...props} />
+
+                    <InspectorControls group="styles">
+                        <PanelBody title="Dimensions">
+                            <UnitControl
+                                label="Gap"
+                                value={currentGap}
+                                onChange={updateGap}
+                                units={[
+                                    { value: 'px', label: 'px', default: '0' },
+                                    { value: 'em', label: 'em' },
+                                    { value: 'rem', label: 'rem' },
+                                ]}
+                            />
+                        </PanelBody>
+                    </InspectorControls>
+                </Fragment>
+            );
+        };
+    },
+    "withQueryPaginationGapControl"
+);
+
+addFilter(
+    "editor.BlockEdit",
+    "my-plugin/query-pagination-gap-control",
+    withQueryPaginationGapControl
+);
