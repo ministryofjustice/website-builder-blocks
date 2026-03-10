c8033c1b803ab345426285b8a49944cf4cfb09c1
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2026-03-10 16:39:18 +0000

    Tidy up initial work

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '2b5902b7337b2ac4d657');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'b002eb9ae28bee164548');
--- src/extended-core-blocks/list/index.js
+++ src/extended-core-blocks/list/index.js
@@ -1,14 +1,5 @@
-import { InspectorControls } from "@wordpress/block-editor";
 import { registerBlockStyle } from "@wordpress/blocks";
-import {
-  PanelBody,
-  PanelRow,
-  ToggleControl,
-  __experimentalVStack as VStack,
-} from "@wordpress/components";
-import { RawHTML } from "@wordpress/element";
-import { addFilter } from "@wordpress/hooks";
-import { __, sprintf } from "@wordpress/i18n";
+import { __ } from "@wordpress/i18n";
 
 /**
  * Register our custom block style.
