2ffe6f6d2e0067698bda66e9eed07e853307f448
adam <brown-a2@users.noreply.github.com>
2026-07-03 12:59:50 +0100

    Fix child dependency warnings

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,24 @@ const glob = require('glob');
 // Load the full lodash build.
 const _ = require('lodash');
 
+// Sass compiler options.
+// quietDeps: mute deprecation warnings from node_modules deps (govuk-frontend etc.) we can't fix.
+// silenceDeprecations: mute @import + slash-division deprecations until our own scss is migrated to @use.
+const sassOptions = {
+    sassOptions: {
+        precision: 8,
+        outputStyle: 'expanded',
+        quietDeps: true,
+        silenceDeprecations: [
+            'import',
+            'slash-div',
+            'global-builtin',
+            'color-functions',
+            'mixed-decls',
+        ],
+    },
+};
+
 mix_.webpackConfig({
     module: {
         rules: [
@@ -19,8 +37,8 @@ mix_.webpackConfig({
 })
 
 .setPublicPath('build/')
-.sass('style.scss', 'style-frontend.min.css')
-.sass('style-gutenburg.scss', 'style-gutenburg.min.css')
+.sass('style.scss', 'style-frontend.min.css', sassOptions)
+.sass('style-gutenburg.scss', 'style-gutenburg.min.css', sassOptions)
 .copy('./node_modules/@ministryofjustice/frontend/moj/all.js', 'build/moj-frontend.js')
 
 const frontendFiles = glob.sync('src/**/frontend.js');
