0ef4d58da6905f94ef0a69bf69cc1ac0ac15f520
Debra Constantinou <debra.constantinou@justice.gov.uk>
2026-07-22 16:01:09 +0100

    test: add intital Jest test set up

---
+++ src/custom-blocks/allowed-third-party-embed/test/index.test.js
@@ -0,0 +1,7 @@
+//unit tests for allowed third party embed block - editor 
+
+describe( "Allowed third-party embed block", () => {
+	it( "runs initial test", () => {
+		expect( true ).toBe( true );
+	} );
+} );
