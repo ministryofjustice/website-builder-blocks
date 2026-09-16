2f94347c597e359c0728ac6a8c9ab36fa53c2440
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2026-09-16 15:28:16 +0100

    Added page count to listing block

--- src/custom-blocks/filterable-listing/preview-filter.js
+++ src/custom-blocks/filterable-listing/preview-filter.js
@@ -5,7 +5,6 @@ export default function Preview({ attributes, acfFields, taxonomies }) {
 	if (attributes.variant !== "default") return null;
 
 	const selectedAcfFields = acfFields.filter(field => attributes.listingDisplayFields.includes(field.key));
-
 	const fieldLabels = {
 		title: {
 			label: "Title",
@@ -192,7 +191,7 @@ export default function Preview({ attributes, acfFields, taxonomies }) {
 						className={`wb-listing wbb:mb-4 wbb:pb-2 ${attributes.stylesResultsShadedBackground ? "" : "wbb:border-b"}`}
 						style={{ borderColor: attributes.stylesResultsBorderColour }}
 					>
-						𝑥 items
+						Showing 𝑥 to 𝑦 of 𝑧 items
 					</div>
 					{Array.from({ length: attributes.listingItemsPerPage }, (_, i) => (
 						<PreviewItems
